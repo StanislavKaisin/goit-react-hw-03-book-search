98d98acac75c8e483c6b041bf5a47366d011227f
Stanislav Kaisin <8050361@gmail.com>
2019-06-16 14:05:59 +0300

    added book search component

---
+++ my-app/src/Components/App/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import BookSearch from '../bookSearch/BookSearch/BookSearch';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <BookSearch></BookSearch>
+    </div>
+  );
+}
+
+export default App;
