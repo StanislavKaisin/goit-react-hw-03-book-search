6de10d7c1ff36164458b8407545843e9e04b393b
Stanislav Kaisin <8050361@gmail.com>
2019-06-16 14:10:52 +0300

    Updates

---
+++ precache-manifest.42eccce2ea876dab80521dc437bf80d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07085119b46916fac770cf5097d37b36",
+    "url": "/goit-react-hw-03-book-search/index.html"
+  },
+  {
+    "revision": "83591b4a48b40f9e49e1",
+    "url": "/goit-react-hw-03-book-search/static/css/main.2b5e2782.chunk.css"
+  },
+  {
+    "revision": "98533bdc3a05497a5601",
+    "url": "/goit-react-hw-03-book-search/static/js/2.20e40e72.chunk.js"
+  },
+  {
+    "revision": "83591b4a48b40f9e49e1",
+    "url": "/goit-react-hw-03-book-search/static/js/main.93722a85.chunk.js"
+  },
+  {
+    "revision": "94506de2c381820c5c24",
+    "url": "/goit-react-hw-03-book-search/static/js/runtime~main.5d5bdee0.js"
+  }
+]);
